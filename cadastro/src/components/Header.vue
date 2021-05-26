5fed9436eda55eddce4f139cdeb4dd89ee69ed59
kettlynmachado <kettlyn.machado@rhfactor.com.br>
2021-05-26 05:15:45 -0300

    niciei o projeto e criei o component header

---
+++ cadastro/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav>
+      <img
+        id="menu-button"
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg"
+        alt="Abrir menu"
+      />
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  background: #17222d;
+  width: 100%;
+  height: 60px;
+}
+
+#menu-button {
+  width: 30px;
+}
+</style>
